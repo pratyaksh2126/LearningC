e2ef58dd2e3a05332fc4030a19a9268574ea87d8
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-10-28 23:18:28 +0530

    Create Day20x1.c

---
+++ Day20x1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double p, r, t, ci;
+    printf("Enter principal, rate and time: ");
+    scanf("%lf %lf %lf", &p, &r, &t);
+    ci = p * pow((1 + r / 100), t) - p;
+    printf("Compound Interest = %.2lf\n", ci);
+    return 0;
+}
