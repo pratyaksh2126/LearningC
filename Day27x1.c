6606592c00f5829fb26c17f790b079e45d8fa454
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-10-28 23:55:50 +0530

    Create Day27x1.c

---
+++ Day27x1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+double power(double base, int exp) {
+    if (exp == 0) return 1;
+    else return base * power(base, exp - 1);
+}
+int main() {
+    double base; int exp;
+    printf("Enter base and exponent: "); scanf("%lf%d", &base, &exp);
+    printf("%.2lf^%d = %.2lf\n", base, exp, power(base, exp));
+    return 0;
+}
