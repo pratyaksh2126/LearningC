f1020065293a6f581149978929940dcaf5eea371
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-10-29 00:15:49 +0530

    Create Day51x2.c

---
+++ Day51x2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+void preorder(struct Node* root) {
+    if (root) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+void inorder(struct Node* root) {
+    if (root) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+void postorder(struct Node* root) {
+    if (root) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    printf("Preorder: ");
+    preorder(root);
+    printf("\nInorder: ");
+    inorder(root);
+    printf("\nPostorder: ");
+    postorder(root);
+    printf("\n");
+    return 0;
+}
