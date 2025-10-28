cd4cd66a80eae313f502f35defd97dc3e8becc58
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-10-28 23:07:14 +0530

    Create Day2x2.c

---
+++ Day2x2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of int: %zu\n", sizeof(int));
+    printf("Size of float: %zu\n", sizeof(float));
+    printf("Size of double: %zu\n", sizeof(double));
+    printf("Size of char: %zu\n", sizeof(char));
+    return 0;
+}
