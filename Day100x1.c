a93a21712dbf2228fb7974e9d3307c397b453afe
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-11-27 13:30:05 +0530

    Create Day100x1.c

---
+++ Day100x1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int arr[50], n, i, j, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n - 1; i++) {
+        for(j = 0; j < n - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
