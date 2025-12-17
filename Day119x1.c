b3f2e789a62b3bcaddb2a1b607030f614c43093c
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-12-17 22:14:58 +0530

    Create Day119x1.c

---
+++ Day119x1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Define structure
+struct Student {
+    int roll;
+    char name[30];
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    // Input
+    printf("Enter Roll Number: ");
+    scanf("%d", &s.roll);
+
+    printf("Enter Name: ");
+    scanf(" %[^\n]", s.name);
+
+    printf("Enter Marks: ");
+    scanf("%f", &s.marks);
+
+    // Output
+    printf("\n--- Student Details ---\n");
+    printf("Roll Number: %d\n", s.roll);
+    printf("Name: %s\n", s.name);
+    printf("Marks: %.2f\n", s.marks);
+
+    return 0;
+}
