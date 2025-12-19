e23cb5a4ea9f965b96ad93938fef85f2c5999d4c
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2025-12-19 22:49:08 +0530

    Create Day121x2.c

---
+++ Day121x2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int isPalindrome(char str[])
+{
+    int i, len = strlen(str);
+    for(i = 0; i < len/2; i++)
+    {
+        if(str[i] != str[len - i - 1])
+            return 0;
+    }
+    return 1;
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    gets(str);
+
+    if(isPalindrome(str))
+        printf("Palindrome String");
+    else
+        printf("Not a Palindrome String");
+
+    return 0;
+}
