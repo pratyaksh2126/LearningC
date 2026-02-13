1a08d6d024d1ea55b30a99116eb0cd75d5e0454e
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-13 10:45:51 +0530

    Create Day179x2.c

---
+++ Day179x2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int *ptr = arr;
+    int max = *ptr;
+
+    for (int i = 1; i < n; i++) {
+        ptr++;
+
+        if (*ptr > max)
+            max = *ptr;
+    }
+
+    printf("%d", max);
+
+    return 0;
+}
