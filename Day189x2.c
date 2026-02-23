8604faebfeebc3967d278be63bdc2160c34fc0a7
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-23 15:29:45 +0530

    Create Day189x2.c

---
+++ Day189x2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int symmetric = 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                symmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (symmetric)
+        printf("Symmetric Matrix");
+    else
+        printf("Not Symmetric Matrix");
+
+    return 0;
+}
