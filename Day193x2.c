cf58ee904008ae736677834d70ad2aba41e12d55
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-02-27 17:15:09 +0530

    Create Day193x2.c

---
+++ Day193x2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int nums[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int left[n], right[n], answer[n];
+
+    left[0] = 1;
+    for(int i = 1; i < n; i++) {
+        left[i] = left[i - 1] * nums[i - 1];
+    }
+
+    right[n - 1] = 1;
+    for(int i = n - 2; i >= 0; i--) {
+        right[i] = right[i + 1] * nums[i + 1];
+    }
+
+    for(int i = 0; i < n; i++) {
+        answer[i] = left[i] * right[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", answer[i]);
+    }
+
+    return 0;
+}
