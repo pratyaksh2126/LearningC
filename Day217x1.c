c05911c0cbc7a9e7b11661e999d5a811c09cec01
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-23 22:17:52 +0530

    Create Day217x1.c

---
+++ Day217x1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left, *right;
+};
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void inorder(struct TreeNode* root) {
+    if (!root) return;
+    inorder(root->left);
+    printf("%d ", root->val);
+    inorder(root->right);
+}
+
+int main() {
+    struct TreeNode* root = newNode(1);
+    root->right = newNode(2);
+    root->right->left = newNode(3);
+
+    inorder(root);
+    return 0;
+}
