a3cd82a26ee4c359c51ea342f9bd9417e44d9e88
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-26 10:20:15 +0530

    Create Day220x2.c

---
+++ Day220x2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void inorder(struct Node* root) {
+    if (!root) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    inorder(root);
+    return 0;
+}
