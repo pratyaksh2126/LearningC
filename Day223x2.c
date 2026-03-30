bdc2f8b494562fba1d1aea23697d900c71f2498b
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-30 21:48:19 +0530

    Create Day223x2.c

---
+++ Day223x2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (!root) return createNode(val);
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+    return root;
+}
+
+int findMin(struct TreeNode* root) {
+    while (root->left)
+        root = root->left;
+    return root->val;
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+    int n, x;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    printf("%d", findMin(root));
+
+    return 0;
+}
