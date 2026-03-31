2b67a4d93a0dee393c7d69cdecde6061b94e7bbb
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-03-31 22:53:56 +0530

    Create Day224x4.c

---
+++ Day224x4.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (!root) return newNode(val);
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+    return root;
+}
+
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if (!root || root->val == val)
+        return root;
+    if (val < root->val)
+        return searchBST(root->left, val);
+    return searchBST(root->right, val);
+}
+
+void inorder(struct TreeNode* root) {
+    if (!root) return;
+    inorder(root->left);
+    printf("%d ", root->val);
+    inorder(root->right);
+}
+
+int main() {
+    int n, x, val;
+    struct TreeNode* root = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    scanf("%d", &val);
+
+    struct TreeNode* res = searchBST(root, val);
+
+    if (!res)
+        printf("Not Found");
+    else
+        inorder(res);
+
+    return 0;
+}
