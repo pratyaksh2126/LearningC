0cb3dde5de061baa0afa37a080a2830d875a796a
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-04-08 12:21:23 +0530

    Create Day232x1.c

---
+++ Day232x1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+void levelOrder(struct Node* root) {
+    if (!root) return;
+
+    struct Node* queue[1000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        struct Node* temp = queue[front++];
+        printf("%d ", temp->data);
+
+        if (temp->left) queue[rear++] = temp->left;
+        if (temp->right) queue[rear++] = temp->right;
+    }
+}
+
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    levelOrder(root);
+    return 0;
+}
