008b136a244eda75966b7643d0a10dbb102268be
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-04-26 21:48:52 +0530

    Create Day250x1.c

---
+++ Day250x1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int searchInsert(int arr[], int n, int target) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    return low;
+}
+
+int main() {
+    int n, target;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &target);
+
+    printf("%d", searchInsert(arr, n, target));
+
+    return 0;
+}
