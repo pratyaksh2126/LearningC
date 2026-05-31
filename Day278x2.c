a51ef1eaf4f60ac7fc4a2b3280d503517d5bf42b
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-05-31 23:54:49 +0530

    Create Day278x2.c

---
+++ Day278x2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, max;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    max = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
