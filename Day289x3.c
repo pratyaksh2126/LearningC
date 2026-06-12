5dbb31b0284e61063d3a4d7c5dd22373a262157a
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-06-12 14:34:17 +0530

    Create Day289x3.c

---
+++ Day289x3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, largest;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0];
+
+    for (i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("Largest element = %d", largest);
+
+    return 0;
+}
