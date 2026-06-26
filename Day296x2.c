fee324c6a221507a5a1e95b2a5dfa60f5860267a
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-06-27 01:03:54 +0530

    Create Day296x2.c

---
+++ Day296x2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int largest = arr[0];
+    int smallest = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > largest)
+            largest = arr[i];
+
+        if(arr[i] < smallest)
+            smallest = arr[i];
+    }
+
+    printf("Largest = %d\n", largest);
+    printf("Smallest = %d\n", smallest);
+
+    return 0;
+}
