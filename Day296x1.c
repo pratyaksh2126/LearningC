06603d398ece605fee48bf5b1b8c334c4f3583d3
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-06-27 01:03:51 +0530

    Create Day296x1.c

---
+++ Day296x1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0, i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        if(ch >= 'A' && ch <= 'Z')
+            ch = ch + 32;
+
+        if(ch >= 'a' && ch <= 'z') {
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+        else if(ch >= '0' && ch <= '9')
+            digits++;
+        else if(ch == ' ')
+            spaces++;
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d\n", consonants);
+    printf("Digits = %d\n", digits);
+    printf("Spaces = %d\n", spaces);
+
+    return 0;
+}
