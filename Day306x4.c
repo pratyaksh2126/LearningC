99e1d333570be33bc7c82395db282e8e482e148c
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-07-14 23:59:01 +0530

    Create Day306x4.c

---
+++ Day306x4.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+    int num, rev = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    while(num != 0) {
+        rev = rev * 10 + num % 10;
+        num /= 10;
+    }
+
+    printf("Reversed Number = %d", rev);
+
+    return 0;
+}
