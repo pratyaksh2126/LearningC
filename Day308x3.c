24e088b69ab0abd3064fdf63c128508b23b25c22
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-07-17 22:13:41 +0530

    Create Day308x3.c

---
+++ Day308x3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i = 0, vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+    printf("Enter a string:\n");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[i] != '\0') {
+        char ch = str[i];
+
+        if(ch >= 'A' && ch <= 'Z')
+            ch += 32;
+
+        if(ch >= 'a' && ch <= 'z') {
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+        else if(ch >= '0' && ch <= '9')
+            digits++;
+        else if(ch == ' ')
+            spaces++;
+
+        i++;
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d\n", consonants);
+    printf("Digits = %d\n", digits);
+    printf("Spaces = %d\n", spaces);
+
+    return 0;
+}
