166c2bca09889f34cf4d232c0e45df994dd387ae
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-07-24 20:37:25 +0530

    Create Day313x4.c

---
+++ Day313x4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &r, &c);
+
+    int A[r][c], B[r][c], C[r][c];
+
+    printf("Enter First Matrix:\n");
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d",&A[i][j]);
+
+    printf("Enter Second Matrix:\n");
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d",&B[i][j]);
+
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            C[i][j]=A[i][j]+B[i][j];
+
+    printf("Result Matrix:\n");
+
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++)
+            printf("%d ",C[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
