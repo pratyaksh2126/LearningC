3162cdf9c9028a0c6902a529ac046ec5909164f2
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-08-01 20:19:57 +0530

    Create Day318x4.c

---
+++ Day318x4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3], b[3][3], c[3][3];
+
+    printf("Enter first matrix:\n");
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter second matrix:\n");
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            scanf("%d", &b[i][j]);
+
+    printf("Sum Matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
