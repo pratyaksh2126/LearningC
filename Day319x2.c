6fc1898563c8b04495c021c05569c0492fe41243
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-08-03 22:24:03 +0530

    Create Day319x2.c

---
+++ Day319x2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int largest = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > largest)
+            largest = arr[i];
+    }
+
+    printf("Largest element = %d", largest);
+
+    return 0;
