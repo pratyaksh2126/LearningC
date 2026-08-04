53488fc883cf7619c4c76912c273226416206105
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-08-04 15:46:37 +0530

    Create Day320x1.c

---
+++ Day320x1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0, i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+            vowels++;
+        else if(str[i] >= 'a' && str[i] <= 'z')
+            consonants++;
+        else if(str[i] >= '0' && str[i] <= '9')
+            digits++;
+        else if(str[i] == ' ')
+            spaces++;
+    }
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+    printf("Digits: %d\n", digits);
+    printf("Spaces: %d\n", spaces);
+
+    return 0;
+}
