8c8c0606eda00491478a48d831e34596d18cbd70
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-08-15 22:31:02 +0530

    Create Day331x4.c

---
+++ Day331x4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+struct Student {
+    int roll;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student s[5];
+    int n, i;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("\nEnter details of student %d:\n", i + 1);
+
+        printf("Roll number: ");
+        scanf("%d", &s[i].roll);
+
+        printf("Name: ");
+        scanf("%s", s[i].name);
+
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+
+    printf("\nStudent Records:\n");
+
+    for (i = 0; i < n; i++) {
+        printf("\nRoll: %d", s[i].roll);
+        printf("\nName: %s", s[i].name);
+        printf("\nMarks: %.2f\n", s[i].marks);
+    }
+
+    return 0;
+}
