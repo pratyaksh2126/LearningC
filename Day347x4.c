03a2a893f137d80988f39749df3c696120504abd
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-08-30 00:30:18 +0530

    Create Day347x4.c

---
+++ Day347x4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int t1 = 0, t2 = 1, next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: %d %d ", t1, t2);
+
+    for(i = 3; i <= n; i++) {
+        next = t1 + t2;
+        printf("%d ", next);
+        t1 = t2;
+        t2 = next;
+    }
+
+    return 0;
+}
