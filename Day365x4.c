2671e2c3406890492300058a05214e0d21328769
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-09-19 22:57:48 +0530

    Create Day365x4.c

---
+++ Day365x4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
