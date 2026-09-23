38e2950e77dc051cf7e3366402ee01283f80baf1
pratyaksh2126 <pratyakshsingh2621@gmail.com>
2026-09-23 23:09:32 +0530

    Create Day369x1.c

---
+++ Day369x1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if(n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    printf("Factorial = %d", factorial(num));
+    return 0;
+}
